e5357f719835beb6e1a3314fb86dde0b3e7611ed
curtis nakauye <curtis@curtiss-MBP.routerlogin.net>
2018-12-11 15:36:41 -0800

    simple component class

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import Foo from './components/foo'
+
+let foo = new Foo("Yo curt!")
+
+console.log("foo: ", foo.message);
---
+++ src/components/foo.ts
@@ -0,0 +1,5 @@
+export default class Foo {
+    constructor (public message:string) {}
+
+    logMessage = () => this.message
+}
